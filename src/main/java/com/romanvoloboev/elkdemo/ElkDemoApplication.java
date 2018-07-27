ece3e882ae4f75e3978d99ec4df577e02c92709f
Roman Voloboev <romanvoloboev@gmail.com>
2018-07-27 19:23:31 +0300

    ....

--- src/main/java/com/romanvoloboev/elkdemo/ElkDemoApplication.java
+++ src/main/java/com/romanvoloboev/elkdemo/ElkDemoApplication.java
@@ -1,7 +1,10 @@
 package com.romanvoloboev.elkdemo;
 
+import net.logstash.logback.encoder.LogstashEncoder;
+import net.logstash.logback.fieldnames.LogstashFieldNames;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,13 +13,33 @@ public class ElkDemoApplication {
 
     private static Logger logger = LoggerFactory.getLogger(ElkDemoApplication.class.getName());
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SpringApplication.run(ElkDemoApplication.class, args);
 
-        logger.info("Test hello");
-        logger.info("Test hello");
-        logger.info("Test hello");
-        logger.info("Test hello");
-        logger.info("Test hello");
+//        LogstashEncoder logstashEncoder = new LogstashEncoder();
+//
+//        LogstashFieldNames logstashFieldNames = new LogstashFieldNames();
+//        logstashFieldNames.setStackTrace("");
+//
+//        logstashEncoder.setFieldNames();
+
+
+
+        do {
+            MDC.put("user.id", "1");
+            MDC.put("user.name", "userName_kek");
+            MDC.put("service.name", "test-app");
+            MDC.put("service.node", "1");
+            logger.info("Info test");
+            try {
+                throw new IllegalArgumentException("exception message");
+            } catch (Exception e) {
+                logger.error("some exception", e);
+            }
+            Thread.sleep(5000);
+            MDC.clear();
+        } while (true);
+
+
     }
 }
